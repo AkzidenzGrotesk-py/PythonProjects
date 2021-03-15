e06676c33d3e16a803979ee174e03d3a8a47cad5
Sky Coleman <72958909+AkzidenzGrotesk-py@users.noreply.github.com>
2021-03-14 20:58:13 -0700

    Update badlambda.py

--- badlambda/badlambda.py
+++ badlambda/badlambda.py
@@ -5,3 +5,7 @@ matter = lambda m, d, v: (d * v) if not m else (m / v) if not d else (m / d)
 # Use
 # getcontents(url) - url set to url, will return the contents of the webpage
 import urllib3;from bs4 import BeautifulSoup;getcontents = lambda url:BeautifulSoup(urllib3.PoolManager().request('GET', url).data, 'html.parser')
+
+# Generate (pseudo) random numbers using Lehmer and Wyhash (very bad, see lemire.py)
+wyhash32 = lambda seed: (((((seed * 0x4a39b70d) >> 32) ^ (seed * 0x4a39b70d)) * 0x12fad5c9) >> 32) ^ ((((seed * 0x4a39b70d) >> 32) ^ (seed * 0x4a39b70d)) * 0x12fad5c9)
+lehmer32 = lambda seed: (seed * 0xe120fc15) >> 32
