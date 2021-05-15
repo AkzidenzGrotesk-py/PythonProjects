30b98ae39b97e41f794b3a87435e1ebc11c3754a
Sky Coleman <72958909+AkzidenzGrotesk-py@users.noreply.github.com>
2021-05-15 16:03:40 -0700

    Update badlambda.py

--- misc/badlambda/badlambda.py
+++ misc/badlambda/badlambda.py
@@ -14,3 +14,15 @@ lehmer32 = lambda seed: (seed * 0xe120fc15) >> 32
 from re import sub, compile
 star = lambda c: sub(compile('[\'\,\]\[]|_'), '', str([["*" for i in range(j)] for j in range(1,c+1)]).replace("], ","\n"))
 # print(star(6))
+
+# star v2
+star = lambda c: print("\n".join(["".join(["*" for i in range(j)]) for j in range(1,c+1)]))
+# star(5)
+
+# Roll dice
+import random
+rd = lambda dice, count, pad: print((("d" + str(dice) + (" " * ((pad - 1) - len(str(dice))))) * count) + "\n" + "".join([(i + (" " * (pad - len(i)))) for i in [str(random.randrange(1, dice + 1)) for y in range(count)]]))
+# rd(dice type, number of dice, padding)
+# rd(6, 3, 3)
+# . d6 d6 d6
+# . 3  4  5
