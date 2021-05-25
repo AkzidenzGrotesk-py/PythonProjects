dbeeb5709138086c0755459bcba6ed255b9b7e5f
Sky Coleman <72958909+AkzidenzGrotesk-py@users.noreply.github.com>
2021-05-24 17:58:14 -0700

    Create example.rb

---
+++ console/notruby/example.rb
@@ -0,0 +1,12 @@
+# call me with 'python ruby.py example.rb'
+puts "Hello, world!"
+
+x = 5 * (3 / 2) + (3 + 2 * (2 - 1))
+y = 2 + x
+
+name = "David"
+
+puts name.size
+puts "Hello, " + name.upcase # this will print "HELLO, DAVID"
+puts "Hello, ".upcase + name # this will print "HELLO, David"
+# .size, .upcase and .downcase
