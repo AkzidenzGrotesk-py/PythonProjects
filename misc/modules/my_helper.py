eada228180f805a41c9b1615b1b0cb721e1d4d70
Sky Coleman <72958909+AkzidenzGrotesk-py@users.noreply.github.com>
2021-12-24 22:41:17 -0800

    Update my_helper.py

--- misc/modules/my_helper.py
+++ misc/modules/my_helper.py
@@ -8,8 +8,12 @@ generate_around:    generate values "around" (and within bounds of) an x, y posi
 caret_H:            quickly reset the cursor
 construct_mat:      generate a matrix width by height with value
 shape_mat:          generate a matrix with shape, filled with value
+render_mat:         render 2d matrix mat as a grid
+size_mat:           get the width and height of mat
 print_len:          print object alongside its len()
-file_get:           get the contents of a file
+file_rget:          get the contents of a file
+file_lget:          get the contents of a file as a list of stripped* newlines
+file_mget:          get the contents of a file as a matrix of cells
 unduplicate:        remove duplicate values out of a list
 flatten:            2d matrix into 1d list'''
 from itertools import chain
@@ -22,7 +26,7 @@ DIRS_3x3 = [(-1, -1), (0, -1), (1, -1),
            (-1, 0),   (0, 0),  (1, 0),
            (-1, 1),   (0, 1),  (1, 1)]
 
-
+# ---
 def checkxy_oob(x: int, y: int, lx: int, ly: int) -> bool:
     '''Check if value x OR y is OOB,
     which is defined as lx - 1|ly - 1 > x|y > 0.
@@ -41,11 +45,15 @@ def generate_around(x: int, y: int, lx: int, ly: int, around: list[tuple]) -> li
         for mx, my in around
         if not checkxy_oob(x + mx, y + my, lx, ly)
     ]
+# ---
 
+# ---
 def caret_H(x: int = 0, y: int = 0) -> None:
     '''Move text cursor in console.'''
     print(f"\033[{y};{x}H", end = "")
+# ---
 
+# ---
 def construct_mat(width: int, height: int, value: int = 0) -> list[list]:
     '''Construct a matrix width by height containing value.'''
     return [[value for _ in range(width)] for _ in range(height)]
@@ -54,20 +62,50 @@ def shape_mat(mat: list[list], value: int = 0) -> list[list]:
     '''Construct a matrix with the same shape as mat'''
     return [[value for _ in line] for line in mat]
 
+def render_mat(mat: list[list], cur_action: str = "\033[H"):
+    '''Render a matrix of 1-width values'''
+    print(cur_action + "\n".join([
+        "".join(row)
+        for row in mat
+    ]))
+
+def size_mat(mat: list[list]):
+    '''Returns width and height of matrix'''
+    return len(mat[0]), len(mat)
+
+def flatten_mat(mat: list[list]) -> list:
+    '''Turn a matrix into a list'''
+    return list(chain.from_iterable(mat))
+# ---
+
+# ---
+def unduplicate_list(thing: list) -> list:
+    '''Remove duplicates out of list'''
+    return list(dict.fromkeys(thing))
+# ---
+
+# ---
 def print_len(thing) -> None:
     '''Print thing with the length'''
     print(f"({len(thing)}) {thing}")
+# ---
 
-def file_get(filename: str) -> str:
+# ---
+def file_rget(filename: str) -> str:
     '''Get contents of a file'''
     with open(filename, "r", encoding = "utf-8") as file:
         contents = file.read()
     return contents
 
-def unduplicate(thing: list) -> list:
-    '''Remove duplicates out of list'''
-    return list(dict.fromkeys(thing))
+def file_lget(filename: str) -> str:
+    '''Get contents of file split by lines and strip them'''
+    with open(filename, "r", encoding = "utf-8") as file:
+        contents = file.read().strip().split("\n")
+    return contents
 
-def flatten(thing: list[list]) -> list:
-    '''Turn a matrix into a list'''
-    return list(chain.from_iterable(thing))
+def file_mget(filename: str) -> str:
+    '''Get contents of file as a matrix of cells'''
+    with open(filename, "r", encoding = "utf-8") as file:
+        contents = [list(l.strip()) for l in file.readlines()]
+    return contents
+# ---
